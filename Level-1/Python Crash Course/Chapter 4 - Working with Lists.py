1a78cd8ff28e547b41bf703235ffa18ae2e76782
KamiKaramazov <ahmettalhasen01@gmail.com>
2024-03-10 14:40:54 +0300

    update

--- Level-1/Python Crash Course/Chapter 4 - Working with Lists.py
+++ Level-1/Python Crash Course/Chapter 4 - Working with Lists.py
@@ -48,3 +48,111 @@ numbers = list(range(1, 1000001))
 print("Minimum number:", min(numbers))
 print("Maximum number:", max(numbers))
 print("Sum of all numbers:", sum(numbers))
+
+### 4-6. Odd Numbers: 
+# Use the third argument of the range() function to make a list of the odd numbers from 1 to 20. Use a for loop to print each number.
+
+for number in range(1, 21, 1):
+    print(number)
+
+### 4-7. Threes: 
+# Make a list of the multiples of 3 from 3 to 30. Use a for loop to print the numbers in your list.
+
+for number in range(3,31,3):
+    print(number)
+
+### 4-8. Cubes: 
+# A number raised to the third power is called a cube. For example, the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes (that is, the cube of each integer from 1 through 10), and use a for loop to print out the value of each cube.
+
+cube = []
+for number in range(1,11):
+    cube = number ** 3
+    print(cube)
+
+### 4-9. Cube Comprehension: 
+# Use a list comprehension to generate a list of the first 10 cubes.
+
+cube = [number ** 3 for number in range(1,11)]
+print(cube)
+
+### 4-10. Slices: 
+# Using one of the programs you wrote in this chapter, add several lines to the end of the program that do the following:
+# • Print the message The first three items in the list are:. Then use a slice to print the first three items from that program’s list.
+# • Print the message Three items from the middle of the list are:. Use a slice to print three items from the middle of the list.
+# • Print the message The last three items in the list are:. Use a slice to print the last three items in the list.
+
+pizzas_list = ["Pizzazz Pizzeria", "Bella Napoli Bites", "Rustica Pizza Kitchen"]
+
+def first_three():
+        print("The first three items in the list are:")
+        for pizza in pizzas_list[:3]:
+            print(pizza)
+
+first_three()
+
+def middle_pizza():
+        print("Three items from the middle of the list are:")
+        middle_index = len(pizzas_list) // 2
+        middle_slice = pizzas_list[middle_index - 1: middle_index + 2]
+        print(middle_slice)
+
+middle_pizza()
+
+def last_pizza():
+        print("The last three items in the list are:")
+        for pizza in pizzas_list[-3:]:
+            print(pizza)
+
+last_pizza()
+
+### 4-11. My Pizzas, Your Pizzas: 
+# Start with your program from Exercise 4-1 (page 56). Make a copy of the list of pizzas, and call it friend_pizzas. Then, do the following:
+# • Add a new pizza to the original list.
+# • Add a different pizza to the list friend_pizzas.
+# • Prove that you have two separate lists. Print the message My favorite pizzas are:, and then use a for loop to print the first list. Print the message My friend’s favorite pizzas are:, and then use a for loop to print the second list. Make sure each new pizza is stored in the appropriate list.
+
+# Original list of pizzas
+my_pizzas = ["Pizzazz Pizzeria", "Bella Napoli Bites", "Rustica Pizza Kitchen"]
+
+friend_pizzas = my_pizzas.copy()
+
+my_pizzas.append("Margherita Pizza")
+
+friend_pizzas.append("Pepperoni Pizza")
+
+print("My favorite pizzas are:")
+for pizza in my_pizzas:
+    print(pizza)
+
+print("\nMy friend’s favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(pizza)
+
+### 4-13. Buffet: 
+# A buffet-style restaurant offers only five basic foods. Think of five simple foods, and store them in a tuple.
+# • Use a for loop to print each food the restaurant offers.
+# • Try to modify one of the items, and make sure that Python rejects the change.
+# • The restaurant changes its menu, replacing two of the items with different foods. Add a line that rewrites the tuple, and then use a for loop to print each of the items on the revised menu.
+
+from os import replace
+
+
+simple_foods = ("apple", "banana", "bread", "cheese", "rice")
+
+def each_food():
+    for food in simple_foods:
+        print(food)
+
+each_food()
+
+try:
+    simple_foods[0] = "orange"
+except TypeError as e:
+    print("Error:", e)
+
+revised_menu = ("orange", "mango", "bread", "cheese", "spaghetti")
+
+print("Revised menu:")
+for food in revised_menu:
+    print(food)
+
