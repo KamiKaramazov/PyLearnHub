0e565796aa174f2fde8fae9429d006b7e83aebe5
KamiKaramazov <ahmettalhasen01@gmail.com>
2024-03-12 16:53:52 +0300

    update

--- Level-1/Python Crash Course/Chapter 6 - Dictionaries.py
+++ Level-1/Python Crash Course/Chapter 6 - Dictionaries.py
@@ -186,6 +186,9 @@ for city, info in cities.items():
         print(f"\t{key.capitalize()}: {value}")
     print() 
 
+### 6-12. Extensions: 
+# We’re now working with examples that are complex enough that they can be extended in any number of ways. Use one of the example programs from this chapter, and extend it by adding new keys and values, changing the context of the program or improving the formatting of the output
+
 cities = {
     'Tokyo': {
         'country': 'Japan',
